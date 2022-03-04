e098e5431fe314cb8e4c034e5850c35372d00ee3
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2022-03-04 05:15:24 +0300

    This commit creates the view functions

---
+++ employee/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render,redirect
+from .models import Employee
+from .forms import *
+
+def employees_list(request):
+    employees = Employee.objects.order_by('-id')
+    context = {
+        'employees': employees,
+    }
+    return render(request, 'list.html', context)
+
+def create_employee(request):
+    form = EmployeeForm()
+
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employees-list')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'create.html', context)
+
+def edit_employee(request, id):
+    employee = Employee.objects.get(id=id)
+    form = EditEmployeeForm(instance=employee)
+
+    if request.method == 'POST':
+        form = EditEmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employees-list')
+
+    context = {
+        'employee': employee,
+        'form': form,
+    }
+    return render(request, 'edit.html', context)
+
+def delete_employee(request, id):
+    employee = Employee.objects.get(id=id)
+
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employees-list')
+
+    context = {
+        'employee': employee,
+    }
+    return render(request, 'delete.html', context)
